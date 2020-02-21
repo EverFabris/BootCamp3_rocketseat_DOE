03bf54cb3df2dbae846c2ffff7e0f81413083864
Everton Roberto Fabris <evertonrobertofabris@gmail.com>
2020-02-20 22:19:46 -0300

    continuado a configuração do server

--- server.js
+++ server.js
@@ -1,8 +1,51 @@
 const express = require("express")
 const server = express()
 
+server.use(express.static("public"))
+
+server.use(express.urlencoded({extended:true}))
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("./", {
+    express: server,
+    nocache: true
+})
+
+
+const donors = [
+    {
+        name: "Everton Fabris",
+        blood: "A+"
+    },
+    {
+        name: "Aline Ferruci",
+        blood: "B+"
+    },
+    {
+        name: "Felipe Dilon",
+        blood: "AB+"
+    },
+    {
+        name: "Jacinto",
+        blood: "O-"
+    }
+]
+
 server.get("/", function(req, res){
-    return res.send("Olá Mundo!node se")
+    return res.render("index.html", {donors})
+})
+
+server.post("/", function(req,res){
+    const name = req.body.name
+    const email = req.body.email
+    const blood = req.body.blood
+
+    donors.push({
+        name: name,
+        blood: blood,
+    })
+
+    return res.redirect("/")
 })
 
 server.listen(3000, function(){
