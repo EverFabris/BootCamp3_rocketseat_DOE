3a955d0af44a7b470cfdd252bcde903d22368773
Everton Roberto Fabris <evertonrobertofabris@gmail.com>
2020-02-26 21:22:49 -0300

    Conexao com o bando de dados postgresql;

--- server.js
+++ server.js
@@ -11,7 +11,16 @@ nunjucks.configure("./", {
     nocache: true
 })
 
-
+// configurar banco de dados
+const Pool = require("pg").Pool
+const db = new Pool({
+    user: 'postgres',
+    password: '123',
+    host: 'localhost',
+    port: 5432,
+    database: 'Doadores'
+})
+/*
 const donors = [
     {
         name: "Everton Fabris",
@@ -29,10 +38,14 @@ const donors = [
         name: "Jacinto",
         blood: "O-"
     }
-]
+]*/
 
 server.get("/", function(req, res){
-    return res.render("index.html", {donors})
+    db.query(`SELECT * FROM public."Doadores"`, function(err, result){
+        if (err) return res.send("Erro de banco ARGH")
+        const donors = result.rows
+        return res.render("index.html", { donors })
+    })
 })
 
 server.post("/", function(req,res){
@@ -40,12 +53,20 @@ server.post("/", function(req,res){
     const email = req.body.email
     const blood = req.body.blood
 
-    donors.push({
-        name: name,
-        blood: blood,
-    })
+    if (name == "" || email == "" || blood == ""){
+        return res.send("Todos os campos são obrigatórios.")
+    }
+    // colocar valores novos no banco
+    const query = `INSERT INTO public."Doadores" ("name", "email", "blood") VALUES ($1, $2, $3)`
 
-    return res.redirect("/")
+    const values = [name, email, blood]
+
+    db.query(query, values, function(err){
+        if (err)
+            return res.send(err.message)
+        //fluxo ideal
+        return res.redirect("/")
+    })
 })
 
 server.listen(3000, function(){
